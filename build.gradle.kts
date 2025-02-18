be0336131e0aba1e5d28caa58eeff67d4b8c2d31
Sergey Lanovoy <sla@kode>
2025-02-18 17:12:38 +0200

    Add dokka plugin and logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+  alias(libs.plugins.dokka) apply false
   alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
 
@@ -18,7 +22,33 @@ allprojects {
 }
 
 subprojects {
+  plugins.withType<KotlinPluginWrapper> {
+    apply(plugin = libs.plugins.dokka.get().pluginId)
+    apply(plugin = "maven-publish")
+
+    val dokkaHtml by tasks.existing(DokkaTask::class)
+
+    val dokkaJar by tasks.creating(org.gradle.jvm.tasks.Jar::class) {
+      group = JavaBasePlugin.DOCUMENTATION_GROUP
+      archiveClassifier.set("javadoc")
+      from(dokkaHtml)
+    }
+
+    val sourcesJar by tasks.creating(org.gradle.jvm.tasks.Jar::class) {
+      archiveClassifier.set("sources")
+      from(project.the<SourceSetContainer>()["main"].allSource)
+    }
+
+    configure<PublishingExtension> {
+      publications.withType<MavenPublication> {
+        artifact(dokkaJar)
+        artifact(sourcesJar)
+      }
+    }
+  }
+
   plugins.withType<MavenPublishPlugin> {
+    apply(plugin = libs.plugins.dokka.get().pluginId)
     apply(plugin = "org.gradle.signing")
 
     configure<PublishingExtension> {
@@ -37,19 +67,50 @@ subprojects {
         }
       }
 
-      publications.create<MavenPublication>("maven") {
+
+      publications.withType<MavenPublication> {
         val versionName: String by project
         val pomGroupId: String by project
         groupId = pomGroupId
         version = versionName
 
-        from(components["java"])
+        afterEvaluate {
+          from(components["java"])
+        }
 
         pom {
-          val pomUrl: String by project
           val pomDescription: String by project
-          url.set(pomUrl)
+          val pomUrl: String by project
+          val pomName: String by project
           description.set(pomDescription)
+          url.set(pomUrl)
+          name.set(pomName)
+          scm {
+            val pomScmUrl: String by project
+            val pomScmConnection: String by project
+            val pomScmDevConnection: String by project
+            url.set(pomScmUrl)
+            connection.set(pomScmConnection)
+            developerConnection.set(pomScmDevConnection)
+          }
+          licenses {
+            license {
+              val pomLicenseName: String by project
+              val pomLicenseUrl: String by project
+              val pomLicenseDist: String by project
+              name.set(pomLicenseName)
+              url.set(pomLicenseUrl)
+              distribution.set(pomLicenseDist)
+            }
+          }
+          developers {
+            developer {
+              val pomDeveloperId: String by project
+              val pomDeveloperName: String by project
+              id.set(pomDeveloperId)
+              name.set(pomDeveloperName)
+            }
+          }
         }
       }
 
